5e867501accc8516a85ffa68628455bb82ba783e
Sohit Yadav <beastgamerr57@gmail.com>
2022-11-04 15:49:54 +0530

    Code to check prime was fixed

--- prime.js
+++ prime.js
@@ -1,17 +1,24 @@
 
 
-function checkPrime(){
+function Primecheck(num){
 
-let factors=0;
+let count=0;
 
-for(let i=0;i<num;i++){
+for(let j=0;j<num;j++){
 
-   if(num%i===0){
-   factors++;
+   if(num%j===0){
+    count++;
  }
    }
+  if(count==2){
+     return true;
+  }
+     return false;
 
-
-  
-
+}
+let ans=Primecheck(15);
+  if(ans==true){
+    console.log(num,"is a Prime Number");
+ }else{
+    console.log(num,"is not a Prime Number");
 }
